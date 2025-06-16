6453f3bd43b3d4679f2e3867ee55790c3dc08add
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-16 17:43:18 -0500

    Update main.dart

--- northside_app/lib/main.dart
+++ northside_app/lib/main.dart
@@ -1,22 +1,42 @@
 // lib/main.dart
 
 import 'package:flutter/material.dart';
-import 'package:northside_app/presentation/home_screen/home_screen.dart'; // This imports the screen we are about to fix.
+import 'package:flutter/services.dart';
+
+// Assuming your project structure has these files.
+import 'core/app_export.dart';
+import 'core/utils/initial_bindings.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((value) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Northside App',
-      // This line tells the app to use your HomeScreen as its starting page.
-      home: HomeScreen(),
+    // Sizer is used for responsive UI.
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        // GetMaterialApp is the root of a GetX application.
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: theme,
+          locale: Get.deviceLocale,
+          fallbackLocale: const Locale('en', 'US'),
+          title: 'northside_app',
+          // This tells GetX how to inject your controllers.
+          initialBinding: InitialBindings(),
+          // This tells GetX where to start the app.
+          initialRoute: AppRoutes.initialRoute,
+          // This gives GetX the list of all pages.
+          getPages: AppRoutes.pages,
+        );
+      },
     );
   }
 }
