e339bb2b1e9190432090b638fc0cc023790fa7ef
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-16 13:48:57 -0500

    Update main.dart

--- northside_app/lib/main.dart
+++ northside_app/lib/main.dart
@@ -1,7 +1,7 @@
 // lib/main.dart
 
 import 'package:flutter/material.dart';
-import 'package:northside_app/presentation/home_screen/home_screen.dart'; // Import the new screen
+import 'package:northside_app/presentation/home_screen/home_screen.dart'; // This imports the screen we are about to fix.
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Northside App',
-      // This line tells the app to use your new HomeScreen as its starting page
+      // This line tells the app to use your HomeScreen as its starting page.
       home: HomeScreen(),
     );
   }
