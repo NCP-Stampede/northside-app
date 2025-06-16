375646e40132225689d870ebb8020ac080e6a7a9
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-16 17:55:59 -0500

    Update app_routes.dart

--- northside_app/lib/core/utils/app_routes.dart
+++ northside_app/lib/core/utils/app_routes.dart
@@ -1,22 +1,19 @@
-// lib/core/utils/app_routes.dart
-
 import 'package:get/get.dart';
-// We will create all these imported files in the next steps.
-import 'package:northside_app/presentation/app_shell/app_shell_screen.dart';
 import 'package:northside_app/presentation/app_shell/app_shell_binding.dart';
+import 'package:northside_app/presentation/app_shell/app_shell_screen.dart';
 
 class AppRoutes {
-  // The app will launch and immediately go to the AppShellScreen.
+  // We define the starting point of the app here.
   static const String initialRoute = '/app_shell';
 
   static List<GetPage> pages = [
     GetPage(
       name: initialRoute,
       page: () => const AppShellScreen(),
-      // The binding injects the necessary controllers for the shell and its children.
+      // The binding provides the necessary controllers to the AppShell.
       binding: AppShellBinding(),
     ),
-    // TODO: Add other pages here later, like detail pages
-    // GetPage(name: '/events_detail', page: () => EventsDetailPage()),
+    // Later, you can add routes to detail pages here, for example:
+    // GetPage(name: '/homecoming_details', page: () => HomecomingDetailPage()),
   ];
 }
