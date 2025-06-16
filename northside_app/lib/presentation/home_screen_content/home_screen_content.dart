166af1b794c2a242bde7f1d2b51cce8d91d42886
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-16 17:59:09 -0500

    Update home_screen_content.dart

--- northside_app/lib/presentation/home_screen_content/home_screen_content.dart
+++ northside_app/lib/presentation/home_screen_content/home_screen_content.dart
@@ -1,41 +1,17 @@
-// lib/presentation/home_screen_content/home_screen_content.dart
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class HomeScreenContent extends StatelessWidget {
-  const HomeScreenContent({super.key});
+class HomeScreenContentController extends GetxController {
+  final PageController pageController = PageController();
+  final RxInt currentPageIndex = 0.obs;
 
   @override
-  Widget build(BuildContext context) {
-    // This page is a Stack to draw its own background behind its content.
-    return Stack(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Color(0xFFE8A1A1), Color(0xFFADC6E6), Colors.white],
-              stops: [0.0, 0.25, 0.4],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-        ),
-        SafeArea(
-          bottom: false,
-          child: Column(
-            children: [
-              _buildHeader(),
-              const SizedBox(height: 20),
-              _buildQuickActions(),
-              const SizedBox(height: 25),
-              Expanded(child: _buildEventsCarousel()),
-              // The PageView and PageIndicator state can be managed internally here.
-            ],
-          ),
-        ),
-      ],
-    );
+  void onClose() {
+    pageController.dispose();
+    super.onClose();
   }
-  
-  // All the private _build methods from before are the same here...
+
+  void onPageChanged(int index) {
+    currentPageIndex.value = index;
+  }
 }
