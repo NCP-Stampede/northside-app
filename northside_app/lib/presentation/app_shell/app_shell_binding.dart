2d8bb4bdc25929af4caa82d33968bef19687858d
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-17 09:44:41 -0500

    Update app_shell_binding.dart

--- northside_app/lib/presentation/app_shell/app_shell_binding.dart
+++ northside_app/lib/presentation/app_shell/app_shell_binding.dart
@@ -1,13 +1,18 @@
 import 'package:get/get.dart';
 import 'package:northside_app/presentation/app_shell/app_shell_controller.dart';
 import 'package:northside_app/presentation/home_screen_content/home_screen_content_controller.dart';
+import 'package:northside_app/presentation/athletics_screen/athletics_controller.dart';
+import 'package:northside_app/presentation/attendance_screen/attendance_controller.dart';
 
 class AppShellBinding extends Bindings {
   @override
   void dependencies() {
-    // Provides the AppShellController to manage nav bar state.
+    // This controller manages the nav bar state and is always available.
     Get.lazyPut(() => AppShellController());
-    // Provides the HomeScreenContentController for the carousel state.
+
+    // These controllers are for the content pages.
     Get.lazyPut(() => HomeScreenContentController());
+    Get.lazyPut(() => AthleticsController());
+    Get.lazyPut(() => AttendanceController());
   }
 }
