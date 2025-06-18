04c9da853913a721d67cf30979ee17642a535e23
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-18 09:46:05 -0500

    Create profile_page.dart

---
+++ northside_app/lib/presentation/home_screen_content/placeholder_pages/profile_page.dart
@@ -0,0 +1,7 @@
+// Placeholder for ProfilePage
+import 'package:flutter/material.dart';
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Center(child: Text('Profile Page')));
+}
