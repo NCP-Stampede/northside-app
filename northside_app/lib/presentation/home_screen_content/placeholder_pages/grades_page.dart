0eadce2cbda45a78b3d4db4d4634cc8ad220841c
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-18 09:45:45 -0500

    Create grades_page.dart

---
+++ northside_app/lib/presentation/home_screen_content/placeholder_pages/grades_page.dart
@@ -0,0 +1,7 @@
+// Placeholder for GradesPage
+import 'package:flutter/material.dart';
+class GradesPage extends StatelessWidget {
+  const GradesPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Center(child: Text('Grades Page')));
+}
