364c100ab3bcb8b3df509be71c3d92f076ad7563
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-18 09:45:13 -0500

    Create attendance_page.dart

---
+++ northside_app/lib/presentation/home_screen_content/placeholder_pages/attendance_page.dart
@@ -0,0 +1,7 @@
+// Placeholder for AttendancePage
+import 'package:flutter/material.dart';
+class AttendancePage extends StatelessWidget {
+  const AttendancePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Center(child: Text('Attendance Page')));
+}
