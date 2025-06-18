bfb1c1a431ed577e1d8b204763709ffe6199a4e8
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-18 09:44:24 -0500

    Create athletics_page.dart

---
+++ northside_app/lib/presentation/home_screen_content/placeholder_pages/athletics_page.dart
@@ -0,0 +1,7 @@
+// Placeholder for AthleticsPage
+import 'package:flutter/material.dart';
+class AthleticsPage extends StatelessWidget {
+  const AthleticsPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Center(child: Text('Athletics Page')));
+}
