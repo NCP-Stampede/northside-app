05861e509a70dfab65ad223e11b823cefc191677
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-18 09:41:31 -0500

    Update app_shell_controller.dart

--- northside_app/lib/presentation/app_shell/app_shell_controller.dart
+++ northside_app/lib/presentation/app_shell/app_shell_controller.dart
@@ -1,10 +1,11 @@
 import 'package:get/get.dart';
 
 class AppShellController extends GetxController {
-  // .obs makes this variable "reactive." The UI will auto-update when it changes.
+  // Reactive variable to hold the selected navigation bar index.
   final RxInt navBarIndex = 0.obs;
 
+  // Function to update the index when a nav item is tapped.
   void changePage(int index) {
     navBarIndex.value = index;
   }
-}
+} 
