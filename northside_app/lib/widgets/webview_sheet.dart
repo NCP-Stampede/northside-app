f8c69932328dc9cf0e233ee02f623ec192e14cd1
ssarveshwaraan <swaminathansarvesh@gmail.com>
2025-06-19 15:40:46 -0500

    Create webview_sheet.dart

---
+++ northside_app/lib/widgets/webview_sheet.dart
@@ -0,0 +1,75 @@
+// lib/widgets/webview_sheet.dart
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewSheet extends StatefulWidget {
+  const WebViewSheet({super.key, required this.url});
+  final String url;
+
+  @override
+  State<WebViewSheet> createState() => _WebViewSheetState();
+}
+
+class _WebViewSheetState extends State<WebViewSheet> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (_) => setState(() => _isLoading = false),
+          onWebResourceError: (error) {
+            // Handle errors gracefully
+            setState(() => _isLoading = false);
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.9,
+      minChildSize: 0.5,
+      maxChildSize: 0.9,
+      builder: (_, controller) {
+        return Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFFF2F2F7),
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          child: Column(
+            children: [
+              // Drag handle
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 12),
+                width: 40,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade300,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              // Use a Stack to show a loading indicator over the WebView
+              Expanded(
+                child: Stack(
+                  children: [
+                    WebViewWidget(controller: _controller),
+                    if (_isLoading)
+                      const Center(child: CircularProgressIndicator()),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
