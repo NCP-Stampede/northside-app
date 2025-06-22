d801d07fd08bcbde31689de33c4e66680c1a52f3
Tanmay <mail2tanmaygarg@gmail.com>
2025-06-21 19:30:38 -0500

    backend scraping of maxprep and schedule with mongo collections and threading

---
+++ northside_app/backend/scraping/athletics_roster.py
@@ -0,0 +1,80 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+from bs4 import BeautifulSoup
+import requests
+from backend.models.Athlete import Athlete
+
+def update_athletics_roster():
+    url = "https://www.maxpreps.com/il/chicago/northside-mustangs/"
+    response = requests.get(url)
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    sports = soup.find_all('span', class_="sport-name")
+    sports = [sport.get_text(strip=True).replace("& ", "").replace(" ", "-").lower() for sport in sports]
+    sports = list(set(sports))
+
+    genders = ["girls", "boys"]
+    seasons = ["fall", "winter", "spring"]
+    levels = ["varsity", "jv", "freshman"]
+
+    added_count = 0
+    existing_count = 0
+
+    for sport in sports:
+        for gender in genders:
+            for season in seasons:
+                for level in levels:
+                    # url = f"https://www.maxpreps.com/il/chicago/northside-mustangs/{sport}/{gender}/{level}/{season}/roster/"
+                    temp_url = f"https://www.maxpreps.com/il/chicago/northside-mustangs/{sport}/{gender}/{level}/{season}/24-25/roster/"
+                    response = requests.get(temp_url)
+                    html_content = response.text
+                    soup = BeautifulSoup(html_content, 'html.parser')
+
+                    players = soup.find_all('a', class_="sc-51f90f89-0 hcqeYd name")
+                    players = [player.get_text(strip=True) for player in players]
+                    if players:
+                        # print(players)
+
+                        primary_tds = soup.find_all("td", class_="primary")
+                        grades = []
+                        positions = []
+                        for td in primary_tds:
+                            grade_td = td.find_next_sibling("td")
+                            if grade_td:
+                                grades.append(grade_td.get_text(strip=True))
+                                position_td = grade_td.find_next_sibling("td")
+                                if position_td:
+                                    positions.append(position_td.get_text(strip=True))
+                                else:
+                                    positions.append("N/A")
+                        
+                        # print(grades)
+                        # print(positions)
+                        length = len(players)
+                        for player in players:
+                            existing_athlete = Athlete.objects(
+                                name=player,
+                                sport=sport,
+                                level=level,
+                                gender=gender,
+                                grade=grades[players.index(player)],
+                                position=positions[players.index(player)]
+                            ).first()
+                            if not existing_athlete:
+                                athlete = Athlete(
+                                    name=player,
+                                    sport=sport,
+                                    level=level,
+                                    gender=gender,
+                                    grade=grades[players.index(player)],
+                                    position=positions[players.index(player)]
+                                )
+                                athlete.save()
+                                added_count += 1
+                            else:
+                                existing_count += 1
+
+    print(f"Added {added_count} athletes, existing {existing_count} athletes.")
