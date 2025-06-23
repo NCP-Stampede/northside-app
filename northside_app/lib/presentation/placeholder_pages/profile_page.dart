bde97250d24a59428d09a280cf571d6b10a1b4fd
Sarveshwaraan Swaminathan <swaminathansarvesh@gmail.com>
2025-06-23 14:15:51 -0500

    Update profile_page.dart

--- northside_app/lib/presentation/placeholder_pages/profile_page.dart
+++ northside_app/lib/presentation/placeholder_pages/profile_page.dart
@@ -76,7 +76,7 @@ class ProfilePage extends StatelessWidget {
               onTap: () {
                 if (option.actionType == ProfileActionType.info) {
                   Get.bottomSheet(
-                    const ArticleDetailSheet(article: appInfoArticle),
+                    ArticleDetailSheet(article: appInfoArticle),
                     isScrollControlled: true,
                     backgroundColor: Colors.transparent,
                   );
@@ -95,7 +95,7 @@ class ProfilePage extends StatelessWidget {
                 }
               },
             );
-          }),
+          }).toList(),
           // FIX: The SizedBox and the call to _buildLogoutButton have been removed.
         ],
       ),
