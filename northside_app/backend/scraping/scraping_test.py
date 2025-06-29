506f7c6a6487245975946e41064eef54085c2883
Tanmay <mail2tanmaygarg@gmail.com>
2025-06-29 10:26:36 -0500

    i do need to specify where the driver is.

--- northside_app/backend/scraping/scraping_test.py
+++ northside_app/backend/scraping/scraping_test.py
@@ -13,9 +13,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 
 firefox_options = Options()
 firefox_options.add_argument("--headless")
+service = Service(executable_path="/usr/local/bin/geckodriver", log_path='/dev/null')
 
 def update_athletics_schedule():
     """
@@ -31,7 +33,7 @@ def update_athletics_schedule():
     #     print(f"Error connecting to the database: {e}")
     #     return
 
-    driver = webdriver.Firefox(options=firefox_options)
+    driver = webdriver.Firefox(options=firefox_options, service=service)
     driver.get("https://www.northsideprepathletics.com/schedule?year=2025-2026")
 
     last_height = driver.execute_script("return document.body.scrollHeight")
