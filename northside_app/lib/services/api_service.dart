83ff2124e51224a6f7517c6e8414b0da704265f8
Sarveshwaraan Swaminathan <swaminathansarvesh@gmail.com>
2025-07-21 23:51:25 +0000

    BaseURL points to live API

--- northside_app/lib/services/api_service.dart
+++ northside_app/lib/services/api_service.dart
@@ -5,7 +5,7 @@ import 'package:northside_app/models/sport_data.dart';
 
 class ApiService {
   // The URL where your Python API is deployed
-  static const String baseUrl = 'https://the-live-api-url.com';
+  static const String baseUrl = 'https://api.tgnest.hackclub.app/api';
 
   Future<List<SportEntry>> fetchSportsData() async {
     final response = await http.get(Uri.parse('$baseUrl/api/sports'));
